7676d505ed411ae345b6e44c3a5be341103bf7d0
Zababurin <s.zababurin.v@gmail.com>
2021-11-27 03:18:50 +0300

    Updates

--- tests/service.tests.mjs
+++ tests/service.tests.mjs
@@ -1,11 +1,37 @@
 import utils from '/tests/modules/index.mjs'
 describe('First SW Test Suite', () => {
+    beforeEach(function() {
+        return utils.__testCleanup();
+    });
+
+    after(function() {
+        return utils.__testCleanup();
+    });
+
     it('should test something', () => {
         return new Promise(async (resolve, reject) => {
-            console.log('utils~~~~~~~~~~~',utils )
-            // let url = new URL('./test/proxy/sw.mjs', import.meta.url)
-            // navigator.serviceWorker.register(url, { type: "module", scope: '/newkind-service'})
-            resolve(true)
+            let url = new URL('./proxy/sw.mjs', import.meta.url)
+            navigator.serviceWorker.register(url, { type: "module", scope: '/tests/proxy/'})
+            .then((reg) => {
+               utils.__waitForSWState(reg, 'installed')
+                   .then(data => {resolve()})
+                   .catch(e => reject(e));
+            })
+            .then(() => {
+            return caches.match('/__test/example')
+                .then((response) => {
+                    if (!response) {
+                        throw new Error('Eek, no response was found in the cache.');
+                    }
+
+                    return response.text();
+                })
+                .then((responseText) => {
+                    if (responseText !== 'Hello, World!') {
+                        throw new Error(`The response text was wrong!: '${responseText}'`);
+                    }
+                });
+            })
         })
     });
 });
